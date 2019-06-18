894f38b9eb7fe43941b166f373b24af1bdc1f09d
josh <48034855+JDJoffe@users.noreply.github.com>
2019-06-18 21:59:39 +1000

    move

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Manager
+{
+    #region Var
+
+    //number of lives
+    [Header("Lives")]
+    public int lives = 3;
+    //Respawnpos
+    [Header("Respawn Position")]
+    public Vector3 Respawnpos;
+
+    #endregion
+}
+
+
+
+
+
+
+public static class GameManager
+{
+
+
+    public static void Lives()
+    {
+
+    }
+    public static void Respawn()
+    {
+
+    }
+}
+
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace RubberSumo
+{
+    [RequireComponent(typeof(CharacterController))]
+    public class Player : MonoBehaviour
+    {
+        //movement direction
+        public Vector3 Movement;
+        private CharacterController playercont;
+
+        #region Variables
+
+        [Header("Player Variables")]
+        public float jumpHeight = 10f;
+        public float moveSpeed = 20f;
+        public float Gravity = 20f;
+
+        #endregion
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
